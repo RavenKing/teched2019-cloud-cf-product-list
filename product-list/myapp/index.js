145033e38eaebedb8ac09adc1752c1f5034bc1b2
Yang <kevin.yang02@sap.com>
2020-04-29 19:07:56 +0800

    xs-security

--- product-list/myapp/index.js
+++ product-list/myapp/index.js
@@ -8,11 +8,11 @@ const passport = require('passport');
 const { JWTStrategy } = require('@sap/xssec');
 const xsenv = require('@sap/xsenv');
 
-// // XSUAA Middleware
-// passport.use(new JWTStrategy(xsenv.getServices({uaa:{tag:'xsuaa'}}).uaa));
+// XSUAA Middleware
+passport.use(new JWTStrategy(xsenv.getServices({uaa:{tag:'xsuaa'}}).uaa));
 
-// app.use(passport.initialize());
-// app.use(passport.authenticate('JWT', { session: false }));
+app.use(passport.initialize());
+app.use(passport.authenticate('JWT', { session: false }));
 
 app.get('/products',checkReadScope, getProducts);
  //app.get('/token',checkReadScope, getJWTToken);
@@ -26,18 +26,13 @@ app.get('/orders',checkReadScope, readOrderDetails);
 
 // Scope check
 function checkReadScope(req, res, next) {
-	//prerequiste: JWT or user info should be sent from CAI  
-	//Two ways to check the authorizaton
-	// 1. Check the scope in the JWT token
-	// 2. Check the scope in the body of the request
-	// ABB can choose which approach to do the authorization
-	// 
-	//if (req.authInfo.checkLocalScope('read')) {
+
+	if (req.authInfo.checkLocalScope('read')) {
 		return next();
-	// } else {
-    // 	console.log('Missing the expected scope');
-    // 	res.status(403).end('Forbidden');
-	// }
+	} else {
+    	console.log('Missing the expected scope');
+    	res.status(403).end('Forbidden');
+	}
 }
 
 // Serve static files
